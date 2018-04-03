1dcb4e47fe522eae6e2e038237829d934950732f
HILARION <trejo.hilarion@gmail.com>
2018-04-03 11:44:50 -0300

    firt commit

---
+++ src/app/views/notifications/modals.component.ts
@@ -0,0 +1,17 @@
+import { Component, ViewChild } from '@angular/core';
+import { ModalDirective } from 'ngx-bootstrap/modal';
+
+@Component({
+  templateUrl: 'modals.component.html'
+})
+export class ModalsComponent {
+    public myModal;
+    public largeModal;
+    public smallModal;
+    public primaryModal;
+    public successModal;
+    public warningModal;
+    public dangerModal;
+    public infoModal;
+    public infoModal2;
+}
